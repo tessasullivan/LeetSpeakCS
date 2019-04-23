37530b4132614540afbc22a77b14f17992fc1482
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-04-23 14:57:29 -0700

    Now does not replace s if it is at the beginning of the string

--- LeetSpeak.Tests/LeetSpeakTests.cs
+++ LeetSpeak.Tests/LeetSpeakTests.cs
@@ -7,28 +7,40 @@ namespace LeetSpeak.Tests
     public class LeetSpeakTest
     {
         [TestMethod]
-        public void Translate_LetterEntered_e_ReplacedWith3()
+        public void Translate_LetterProcessed_e_ReplacedWith3()
         {
             LeetSpeak testLeetSpeak = new LeetSpeak();
             Assert.AreEqual("3", testLeetSpeak.Translate("e"));
         }
         [TestMethod]
-        public void Translate_LetterEntered_o_ReplacedWith0()
+        public void Translate_LetterProcessed_o_ReplacedWith0()
         {
             LeetSpeak testLeetSpeak = new LeetSpeak();
             Assert.AreEqual("0", testLeetSpeak.Translate("o"));
         }
         [TestMethod]
-        public void Translate_LetterEntered_I_ReplacedWith1()
+        public void Translate_LetterProcessed_I_ReplacedWith1()
         {
             LeetSpeak testLeetSpeak = new LeetSpeak();
             Assert.AreEqual("1", testLeetSpeak.Translate("I"));
         }
         [TestMethod]
-        public void Translate_LetterEntered_t_ReplacedWith7()
+        public void Translate_LetterProcessed_t_ReplacedWith7()
         {
             LeetSpeak testLeetSpeak = new LeetSpeak();
             Assert.AreEqual("7", testLeetSpeak.Translate("t"));
+        }
+        [TestMethod]
+        public void Translate_LetterProcessedNotAtBeginningOfWord_s_ReplacedWithz()
+        {
+            LeetSpeak testLeetSpeak = new LeetSpeak();
+            Assert.AreEqual("iz", testLeetSpeak.Translate("is"));
+        }
+        [TestMethod]
+        public void Translate_WordStartsWith_s_ReplacedWithz()
+        {
+            LeetSpeak testLeetSpeak = new LeetSpeak();
+            Assert.AreEqual("s37", testLeetSpeak.Translate("set"));
         }
     }
 }
--- LeetSpeak/Models/LeetSpeak.cs
+++ LeetSpeak/Models/LeetSpeak.cs
@@ -15,20 +15,29 @@ namespace LeetSpeak
             char[] inputArray = userInput.ToCharArray();
             for (int i=0; i < inputArray.Length; i++)
             {
-                switch (inputArray[i])
+                if (i == 0 && inputArray[i] == 's')
                 {
-                    case 'e':
-                        inputArray[i] = '3';
-                        break;
-                    case 'o':
-                        inputArray[i] = '0';
-                        break;
-                    case 'I':
-                        inputArray[i] = '1';
-                        break;
-                    case 't':
-                        inputArray[i] = '7';
-                        break;
+                    continue;
+                }
+                else {
+                    switch (inputArray[i])
+                    {
+                        case 'e':
+                            inputArray[i] = '3';
+                            break;
+                        case 'o':
+                            inputArray[i] = '0';
+                            break;
+                        case 'I':
+                            inputArray[i] = '1';
+                            break;
+                        case 't':
+                            inputArray[i] = '7';
+                            break;
+                        case 's':
+                            inputArray[i] = 'z';
+                            break;
+                    }
                 }
             }
             string result = string.Join("", inputArray);
